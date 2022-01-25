e06c889f307cd862613ab729ffa01348a59e8d32
Sai Darshan <saidarshan27@gmail.com>
2022-01-25 12:55:04 +0530

    full functionallu completed

--- scripts/script.js
+++ scripts/script.js
@@ -62,17 +62,29 @@ async function getAllBooks() {
   
     allBooks = data;
 
+    for (let book of allBooks) {
+      const colHTML = generateBookHTML(book);
+      const template = document.createElement("template");
 
-    allBooks.forEach(book => {
-      const col = generateBookHTML(book);
-      
-      row.innerHTML += col;
-    });
+      template.innerHTML = colHTML;
+      const col = template.content.childNodes[1];
+      const cardBody = col.querySelector(".card-body");
+      console.log(cardBody);
+
+      const characters = await getFiveCharactersAsString(book);
+
+      const p = document.createElement("p");
+      p.classList.add("card-subtitle", "mb-2", "text-muted");
+      p.innerText = `Characters - ${(!characters)? 'no characters' : characters}`;
+      cardBody.append(p);
+      row.append(col);
+    }
     
     loadingDiv.remove();
-
     searchContainer.classList.remove("d-none");
     mainContainer.append(row);
+
+
   } catch (error) {
     document.body.append("sorry website not working");
   }
@@ -103,4 +115,24 @@ function generateBookHTML(book) {
     `;
   
   return html;
+}
+
+async function getFiveCharactersAsString(book) {
+  const charactersURLS = book.characters;
+
+  const fiveCharacterNames = [];
+
+  for await (const url of charactersURLS) { 
+    if (fiveCharacterNames.length === 5) {
+      break;
+    }
+    const characterFullDetailResponse = await fetch(url);
+    const characterFullDetail = await characterFullDetailResponse.json();
+
+    if (characterFullDetail.name) {
+      fiveCharacterNames.push(characterFullDetail.name);
+    }
+  }
+
+  return fiveCharacterNames.join(',');
 }
